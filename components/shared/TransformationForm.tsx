063a3f2f9a175d80dcee65eed213ba7ea7bdd4d9
PraveenGeorgeRyan <p.g.ryan333@gmail.com>
2024-02-22 14:42:58 +0530

    errors rectified

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -64,6 +64,7 @@ const TransformationForm = ({
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isTransforming, setIsTransforming] = useState(false);
   const [transformationConfig, setTransformationConfig] = useState(config);
+  // eslint-disable-next-line no-unused-vars
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
 
